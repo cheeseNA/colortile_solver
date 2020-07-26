ab776248a5f487e6fc7b112c126c506ffd064562
cheeseNA <kurocat2000@gmail.com>
2020-07-26 23:30:26 +0900

    [add] added method get_tiles

--- main.py
+++ main.py
@@ -17,6 +17,32 @@ def get_refpoint():
     return max_loc
 
 
+def get_tiles(x, y):
+    tile = np.full((23 + 2, 15 + 2), -1)
+    tile[1:-1, 1:-1] = 0
+
+    np_screen_shot = np.array(ImageGrab.grab())
+    tileimg = np_screen_shot[y + 136:y + 858:50, x + 110:x + 1234:50]
+    Image.fromarray(tileimg).save("tile.png")
+    tileimg = tileimg.transpose(1, 0, 2)
+
+    color_list = np.array([
+        [191, 109, 41], [37, 110, 246], [187, 189, 189], [93, 200, 61],
+        [240, 158, 58], [238, 147, 249], [190, 113, 200], [236, 114, 110],
+        [128, 203, 204], [204, 203, 119]
+    ])
+    for i in range(len(color_list)):
+        tp = np.where(
+            (tileimg[:, :, 0] == color_list[i][0])
+            & (tileimg[:, :, 1] == color_list[i][1])
+            & (tileimg[:, :, 2] == color_list[i][2])
+        )
+        tp = tuple(tp[0] + 1), tuple(tp[1] + 1)
+        tile[tp] = i + 1
+
+    return tile
+
+
 pyautogui.FAILSAFE = True
 
 if __name__ == '__main__':
