d545907b9b3efe7e7558ce8ecbc66b2b9d09e50c
cheeseNA <kurocat2000@gmail.com>
2020-07-27 22:28:07 +0900

    [update] updated method solve

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import cv2
 import pyautogui
-from PIL import ImageGrab, Image
+from PIL import ImageGrab
 
 
 def get_refpoint():
@@ -47,52 +47,48 @@ def get_tiles(x, y):
     return tile
 
 
-def solve(x, y, tile):
-    end_flag = False
-    while not end_flag:
-        end_flag = True
-        il = range(1, 23)
-        jl = range(1, 15)
-        if random.random() < 0.5:
-            il = il[::-1]
-        if random.random() < 0.5:
-            jl = jl[::-1]
-
-        for i in il:
-            for j in jl:
-                if tile[i, j] == 0:
-                    cros = np.zeros(4)
-                    klist = [0, 0, 0, 0]
-                    for k in range(25):
-                        if tile[i - k, j] != 0:
-                            cros[0] = tile[i - k, j]
-                            klist[0] = (i - k, j)
-                            break
-                    for k in range(25):
-                        if tile[i + k, j] != 0:
-                            cros[1] = tile[i + k, j]
-                            klist[1] = (i + k, j)
-                            break
-                    for k in range(25):
-                        if tile[i, j - k] != 0:
-                            cros[2] = tile[i, j - k]
-                            klist[2] = (i, j - k)
-                            break
-                    for k in range(25):
-                        if tile[i, j + k] != 0:
-                            cros[3] = tile[i, j + k]
-                            klist[3] = (i, j + k)
-                            break
-                    dupl = [x for x in range(1, 11) if np.sum(cros == x) >= 2]
-                    if len(dupl) >= 1:
-                        pyautogui.click(
-                            x / 2 + 55 + (i - 1) * 25,
-                            y / 2 + 67 + (j - 1) * 25, interval=0.3
-                        )
-                        for i in range(4):
-                            if cros[i] in dupl:
-                                tile[klist[i][0], klist[i][1]] = 0
-                        end_flag = False
+def solve(x, y, tile, trial_border):
+    trial_counter = 0
+    while trial_counter < trial_border:
+        i = random.randint(1, 23)
+        j = random.randint(1, 15)
+        if tile[i, j] == 0:
+            cros = np.zeros(4)
+            klist = [0, 0, 0, 0]
+            for k in range(25):
+                if tile[i - k, j] != 0:
+                    cros[0] = tile[i - k, j]
+                    klist[0] = (i - k, j)
+                    break
+            for k in range(25):
+                if tile[i + k, j] != 0:
+                    cros[1] = tile[i + k, j]
+                    klist[1] = (i + k, j)
+                    break
+            for k in range(25):
+                if tile[i, j - k] != 0:
+                    cros[2] = tile[i, j - k]
+                    klist[2] = (i, j - k)
+                    break
+            for k in range(25):
+                if tile[i, j + k] != 0:
+                    cros[3] = tile[i, j + k]
+                    klist[3] = (i, j + k)
+                    break
+            dupl = [x for x in range(1, 11) if np.sum(cros == x) >= 2]
+            if len(dupl) >= 1:
+                pyautogui.click(
+                    x / 2 + 55 + (i - 1) * 25,
+                    y / 2 + 67 + (j - 1) * 25, interval=0.3
+                )
+                for i in range(4):
+                    if cros[i] in dupl:
+                        tile[klist[i][0], klist[i][1]] = 0
+                trial_counter = 0
+            else:
+                trial_counter += 1
+        else:
+            trial_counter += 1
 
 
 if __name__ == '__main__':
@@ -100,4 +96,4 @@ if __name__ == '__main__':
     x, y = get_refpoint()
     click_start(x, y)
     tile = get_tiles(x, y)
-    solve(x, y, tile)
+    solve(x, y, tile, 500)
